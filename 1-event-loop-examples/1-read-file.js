9c385926b4e347533205b70035fa8279521a5e6e
Raviegupta <raviegupta153@gmail.com>
2023-08-24 08:53:58 +0530

    event-loop-ex: 1-read-file

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+console.log('started a new task');
+
+fs.readFile('../Content/first.txt', 'utf-8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result);
+    console.log('completed first task');
+})
+console.log('starting next task');
