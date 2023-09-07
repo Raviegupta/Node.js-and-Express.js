bd3fa4f33a1942c1f79fe965e53b041b1c60166e
Raviegupta <raviegupta153@gmail.com>
2023-09-07 21:14:57 +0530

    start express.js

--- 16-streams.js
+++ 16-streams.js
@@ -1,21 +1,24 @@
 const { createReadStream } = require('fs');
 
-// const stream = createReadStream('content/big.txt');
+const stream = createReadStream('content/big.txt', );
+console.log(stream);
 
-// default 64kb
+// by default size of buffer :- 64kb
+// (we can also control it and the way  we do that by passing in the object when we are setting up create read stream, 
+// first argument is path, second arg is object, 'HighWaterMark' is the prop which controls the size of the buffer has the name )
 // last buffer - remainder
 // highWaterMark - control size
 
 
-const stream = createReadStream('content/big.txt', { 
-    highWaterMark: 90000,
-    encoding: 'utf-8',
- })
+// const stream = createReadStream('content/big.txt', { 
+//     highWaterMark: 90000,
+//     encoding: 'utf-8',
+//  })
 
-stream.on('data', (result) => {
-    console.log(result);
-})
+// stream.on('data', (result) => {
+//     console.log(result);
+// })
 
-stream.on('error', (err) => console.log(err));
+// stream.on('error', (err) => console.log(err));
 
 
